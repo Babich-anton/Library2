adda4c5e18c99db176fe2ea7440380461d06df50
Anton Babich <Anton_Babich@epam.com>
2020-12-14 02:56:18 +0300

    Init.

---
+++ Example/Pods/Target Support Files/Library2/Library2-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Library2 : NSObject
+@end
+@implementation PodsDummy_Library2
+@end
---
+++ Main.swift
@@ -0,0 +1,15 @@
+//
+//  Main.swift
+//  Library2
+//
+//  Created by Anton Babich on 12/14/20.
+//
+
+import Library1
+
+public class Main2 {
+    public static func prints() {
+        print("Library 2")
+        Main.prints()
+    }
+}
