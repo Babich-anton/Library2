a79c0685eac819ebe8f0a6e77a9af6bf04c056eb
Anton Babich <Anton_Babich@epam.com>
2020-12-14 02:52:00 +0300

    Initial commit

---
+++ Example/Library2/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Library2
+//
+//  Created by antosha.1998.ru@mail.ru on 12/14/2020.
+//  Copyright (c) 2020 antosha.1998.ru@mail.ru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
