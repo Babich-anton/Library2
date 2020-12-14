6698751d5e5d66c29aaa80e2bceec68bf24a10ab
Anton Babich <Anton_Babich@epam.com>
2020-12-14 03:03:37 +0300

    Updated.

--- Example/Library2/ViewController.swift
+++ Example/Library2/ViewController.swift
@@ -7,18 +7,13 @@
 //
 
 import UIKit
+import Library2
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Main.prints()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
-
