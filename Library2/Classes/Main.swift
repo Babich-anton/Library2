7f372efa7348d8c20919f9cceca3a7eed67d59a8
Anton Babich <Anton_Babich@epam.com>
2020-12-14 10:36:56 +0300

    Updated.

--- Library2/Classes/Main.swift
+++ Library2/Classes/Main.swift
@@ -5,11 +5,11 @@
 //  Created by Anton Babich on 12/14/20.
 //
 
-import Library1
+//import Library1
 
 public class Main2 {
     public static func prints() {
         print("Library 2")
-        Main.prints()
+//        Main.prints()
     }
 }
